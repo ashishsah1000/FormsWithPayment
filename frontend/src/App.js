b75a9a8f7dca839df28d10604fb70bb183c09fa3
ashishsah1000 <ashishsah1000@gamil.com>
2022-07-24 06:41:12 +0530

    added the system file

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Home } from "./pages";
+function App() {
+  return (
+    <div className="App">
+      <div className="mx-auto bg-gray-200 container">
+        <p className="text-gray-500 text-lg"> Hey tailwindcss </p>
+      </div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
